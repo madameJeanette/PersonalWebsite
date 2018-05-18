ea66ac941e782b9d38c27cdc11a83fe1352be966
madameJeanette <danielle_meerburg@live.nl>
2018-05-18 15:20:15 +0200

    Comments toegevoegd.

--- inlog systeem met JS/main.js
+++ inlog systeem met JS/main.js
@@ -1,18 +1,18 @@
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
+var attempt = 3; // hoeveelheid pogingen om fout te hebben
+// de functie hieronder activeert wanner er op de login button wordt geklinkt.
 function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "Admin" && password == "daan12"){
-alert ("Login successfully");
-window.location = "succes.html"; // Redirecting to other page.
-return false;
+var username = document.getElementById("username").value; //selecteert de username tag en zijn value.
+var password = document.getElementById("password").value;  //selecteert de password tag en zijn value.
+if ( username == "Admin" && password == "daan12"){  //vergelijkt of value gelijk zijn aan het wachtwoord en de gebruikersnaam
+alert ("Login successfully");   //geeft pop up met boodschap dat login is gelukt.
+window.location = "succes.html"; // stuurt door naar de pagina wanneer login succesvol.
+return false;  //wanneer values niet gelijk aan ww en acc run deze code.
 }
 else{
-attempt --;// Decrementing by one.
+attempt --;// haalt poging er af bij één.
 alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
+
+if( attempt == 0){// Disabled de invoer velden na drie pogingen.
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
